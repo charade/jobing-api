9a23f4f120eb2508cb79fba3587c42f3fe46c906
Charles.Ek <macbook@MacBook.local>
2021-11-28 15:21:40 +0100

    :sparkles:create new offer

--- src/routes/jobsRoute.js
+++ src/routes/jobsRoute.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
+const job = require('../controllers/jobsController');
 
 router.get('/auth',(req, res, next) => {
     //get connected user jobs
 })
-router.post('/auth/new-job', (req,res,next) => {
+//add a new offer
+router.post('/auth', async(req,res,next) => {
+    const { id } = req;
+    req.body.userId = id;
+    const response = await job.add(req.body);
+    if(response.error){
+        next(response.error);
+        return;
+    }
+    res.status(201).json(response);
     //add a new job
 });
 router.delete('/auth/:jobId', (req, res, next) => {
